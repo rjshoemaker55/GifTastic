7afbfa6c16f3e01f4c24f4f032da0a88d57c322e
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-03-29 22:29:28 -0400

    finished animations, added toggle for keeping/removing existing gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,60 +13,81 @@ var topics = [
 
 function createButtons() {
   $("#buttons-div").empty();
-$.each(topics, function() {
-  var newButton = $("<button>").text(this)
-  newButton.attr("type", "button")
-  newButton.addClass("buttons btn-default btn-primary")
-  $("#buttons-div").append(newButton)
-})
+  $.each(topics, function () {
+    var newButton = $("<button>").text(this)
+    newButton.attr("type", "button")
+    newButton.addClass("buttons btn-default btn-primary")
+    $("#buttons-div").append(newButton)
+  })
 }
 
-$(document.body).on("click", ".buttons", function() {
-  console.log("clicked")
+$(document.body).on("click", ".buttons", function () {
+
   var topicquery = $(this).text()
 
-  console.log(topicquery)
+
   $.ajax({
     url: "http://api.giphy.com/v1/gifs/search?q=" + topicquery + "s&api_key=jSen6BwZSc0sDdHUgWPLxzXznD853orJ&limit=10",
     method: "GET"
-  }).then(function(response) {
-    $("#images-div").empty()
+  }).then(function (response) {
+    if ($("#remove-button").is(":checked")) {
+      $("#images-div").empty()
+    }
+
     for (var i = 0; i <= 9; i++) {
-      console.log(response.data[i].embed_url)
-      var imageURL = (response.data[i].images.fixed_height_still.url)
+      var imageURL = response.data[i].images.fixed_height_still.url
+      var animateURL = response.data[i].images.fixed_height.url
       var rating = response.data[i].rating.toUpperCase();
-
-
-
+      var altText = response.data[i].title
       var card = $("<div>").addClass("card").attr("id", "img-" + i)
 
       $(card).html(`
         <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="${imageURL}" alt="Card image cap">
+        <img class="card-img-top" src="${imageURL}" alt="${altText}" data-still="${imageURL}" data-animate="${animateURL}" data-state="still">
         <div class="card-body">
         <p class="card-text">Rating: ${rating}</p>
         </div>
         </div>
      ` )
 
-      $("#images-div").append(card)
+      $("#images-div").prepend(card)
 
     }
   })
 })
 
-$("#topic-input-button").on("click", function() {
+$("#topic-input-button").on("click", function () {
   event.preventDefault();
 
   var newTopic = $("#topic-input").val();
 
-  topics.push(newTopic)
+  if (newTopic != "") {
 
-  $("#topic-input").val("")
+    topics.push(newTopic)
+    $("#topic-input").val("")
+    createButtons();
 
-  createButtons();
+  } else {
+
+    alert("Please enter a valid topic!")
+
+  }
 
 })
 
+$(document.body).on("click", ".card-img-top", function () {
+
+  if ((this).getAttribute("data-state") == "still") {
+
+    $(this).attr("src", (this).getAttribute("data-animate"))
+    $(this).attr("data-state", "animated")
+
+  } else {
+
+    $(this).attr("src", (this).getAttribute("data-still"))
+    $(this).attr("data-state", "still")
+
+  }
+})
 
 createButtons();
